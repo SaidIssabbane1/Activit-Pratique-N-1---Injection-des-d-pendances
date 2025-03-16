720fdc37d248300073db8b14de21bd84583ab2c4
saidlionsgeeek <saidlionsgeek.1997@gmail.com>
2025-03-16 11:46:14 +0000

    creation d interface metier

---
+++ src/main/java/org/example/metier/IMetier.java
@@ -0,0 +1,5 @@
+package org.example.metier;
+
+public interface IMetier {
+    public void calcul();
+}
---
+++ src/main/java/org/example/metier/metierImpl.java
@@ -0,0 +1,8 @@
+package org.example.metier;
+
+public class metierImpl implements IMetier {
+    @Override
+    public void calcul() {
+
+    }
+}
